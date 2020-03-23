894438a151d878d55bc456490f3a1261509334ed
Pavel Kovalchuk <x5bart@yandex.ru>
2020-03-23 12:03:02 +0300

    decorate starbuzz

--- p03_decorator_starbuzz/src/main/java/com/exampler/a03_decorator_starbuzz/MainActivity.kt
+++ p03_decorator_starbuzz/src/main/java/com/exampler/a03_decorator_starbuzz/MainActivity.kt
@@ -1,39 +1,134 @@
 package com.exampler.a03_decorator_starbuzz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.view.View.*
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val TAG = "Mylog"
+
 class MainActivity : AppCompatActivity() {
 
+    lateinit var beverage: Beverage
+    var btnCondimend = false
+    var sumCost = 0.0
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideCondiments()
 
-        val beverage: Beverage = Espresso()
-        Log.d(TAG,
-            beverage.getDescription()
-                    + " $" + beverage.cost()
-        )
+        btn_darkRoast.setOnClickListener {
+            beverage = DarkRoast()
+            tv_preResult.text = "${beverage.getDescription()} $  ${beverage.cost()}"
+            showCondiments()
+            hideBeverage(btn_darkRoast)
+        }
 
-        var beverage2: Beverage = DarkRoast()
-        beverage2 = Mocha(beverage2)
-        beverage2 = Mocha(beverage2)
-        beverage2 = Whip(beverage2)
-        Log.d(TAG,
-            beverage2.getDescription()
-                    + " $" + beverage2.cost()
-        )
+        btn_decaf.setOnClickListener {
+            beverage = Decaf()
+            tv_preResult.text = "${beverage.getDescription()} $  ${beverage.cost()}"
+            showCondiments()
+            hideBeverage(btn_decaf)
+        }
 
-        var beverage3: Beverage = HouseBlend()
-        beverage3 = Soy(beverage3)
-        beverage3 = Mocha(beverage3)
-        beverage3 = Whip(beverage3)
-        Log.d(TAG,
-            beverage3.getDescription()
-                    + " $" + beverage3.cost()
-        )
+        btn_espresso.setOnClickListener {
+            beverage = Espresso()
+            tv_preResult.text = "${beverage.getDescription()} $  ${beverage.cost()}"
+            showCondiments()
+            hideBeverage(btn_espresso)
+        }
+
+        btn_houseBlend.setOnClickListener {
+            beverage = HouseBlend()
+            tv_preResult.text = "${beverage.getDescription()} $  ${beverage.cost()}"
+            showCondiments()
+            hideBeverage(btn_houseBlend)
+        }
+
+        btn_mocha.setOnClickListener {
+            beverage = Mocha(beverage)
+            tv_preResult.text = "${beverage.getDescription()} $ ${beverage.cost()}"
+        }
+
+        btn_soy.setOnClickListener {
+            beverage = Soy(beverage)
+            tv_preResult.text = "${beverage.getDescription()} $ ${beverage.cost()}"
+        }
+        btn_whip.setOnClickListener {
+            beverage = Whip(beverage)
+            tv_preResult.text = "${beverage.getDescription()} $ ${beverage.cost()}"
+        }
+        btn_addBeverage.setOnClickListener {
+            val preResult = tv_preResult.text.toString()
+            tv_preResult.text = ""
+            val result = tv_result.text.toString()
+            tv_result.text = "$result \n $preResult"
+            hideCondiments()
+            showBeverage()
+            showCost()
+        }
+
+        btn_clear.setOnClickListener {
+            tv_preResult.text = ""
+            tv_result.text = ""
+            tv_sumCost.text = ""
+            hideCondiments()
+        }
 
     }
+
+    private fun showCondiments() {
+        btn_mocha.visibility = VISIBLE
+        btn_soy.visibility = VISIBLE
+        btn_whip.visibility = VISIBLE
+    }
+
+    private fun hideCondiments() {
+        btn_mocha.visibility = GONE
+        btn_soy.visibility = GONE
+        btn_whip.visibility = GONE
+    }
+
+    fun hideBeverage(view: Button) {
+        when (view) {
+            btn_darkRoast -> {
+                btn_decaf.visibility = INVISIBLE
+                btn_espresso.visibility = INVISIBLE
+                btn_houseBlend.visibility = INVISIBLE
+            }
+            btn_decaf -> {
+                btn_darkRoast.visibility = INVISIBLE
+                btn_espresso.visibility = INVISIBLE
+                btn_houseBlend.visibility = INVISIBLE
+            }
+            btn_espresso -> {
+                btn_darkRoast.visibility = INVISIBLE
+                btn_decaf.visibility = INVISIBLE
+                btn_houseBlend.visibility = INVISIBLE
+            }
+            btn_houseBlend -> {
+                btn_darkRoast.visibility = INVISIBLE
+                btn_decaf.visibility = INVISIBLE
+                btn_espresso.visibility = INVISIBLE
+            }
+        }
+    }
+
+    fun showBeverage() {
+        btn_darkRoast.visibility = VISIBLE
+        btn_decaf.visibility = VISIBLE
+        btn_espresso.visibility = VISIBLE
+        btn_houseBlend.visibility = VISIBLE
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun showCost() {
+        sumCost += beverage.cost()
+        tv_sumCost.text = "Cost: \$ $sumCost"
+    }
+
 }
