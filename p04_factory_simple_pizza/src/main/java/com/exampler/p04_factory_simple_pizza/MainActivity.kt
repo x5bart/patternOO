e9262e6695477e7382be1cede61c0de702f2044d
Pavel Kovalchuk <x5bart@yandex.ru>
2020-04-18 13:02:26 +0300

    add p04

---
+++ p04_factory_simple_pizza/src/main/java/com/exampler/p04_factory_simple_pizza/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.exampler.p04_factory_simple_pizza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
